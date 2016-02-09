870b6253f6f9156e9f658a43bf8b9b90ad659b2e
rmhowe <rm.howe@outlook.com>
2016-02-09 11:20:00 +0100

    Improves dragging behaviour, refactors logic into GamePage rather than GameBoard

--- src/js/containers/GamePage.js
+++ src/js/containers/GamePage.js
@@ -9,7 +9,7 @@ import {
   selectPuzzle,
   selectTool
 } from '../actions';
-import { HIGHLIGHTED, EMPTY, HIGHLIGHT, BLOCK } from '../constants';
+import { HIGHLIGHTED, BLOCKED, EMPTY, HIGHLIGHT, BLOCK } from '../constants';
 import GameBoard from '../components/GameBoard';
 import ToolSelector from '../components/ToolSelector';
 import PuzzleSelector from '../components/PuzzleSelector';
@@ -17,11 +17,28 @@ import PuzzleSelector from '../components/PuzzleSelector';
 class GamePage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dragging: false,
+      dragType: null
+    };
+
     this.handleToolChange = this.handleToolChange.bind(this);
-    this.handlePuzzleChange = this.handlePuzzleChange.bind(this);
-    this.handleTileChange = this.handleTileChange.bind(this);
-    this.handleBoardReset = this.handleBoardReset.bind(this);
     this.handleTabPress = this.handleTabPress.bind(this);
+    this.handlePuzzleChange = this.handlePuzzleChange.bind(this);
+    this.handleBoardReset = this.handleBoardReset.bind(this);
+    this.handleBoardDragStart = this.handleBoardDragStart.bind(this);
+    this.handleBoardDrag = this.handleBoardDrag.bind(this);
+    this.handleBoardDragEnd = this.handleBoardDragEnd.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mouseup', this.handleBoardDragEnd);
+    document.addEventListener('mouseleave', this.handleBoardDragEnd);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mouseup', this.handleBoardDragEnd);
+    document.removeEventListener('mouseleave', this.handleBoardDragEnd);
   }
 
   componentDidUpdate(prevProps) {
@@ -29,6 +46,7 @@ class GamePage extends React.Component {
     for (let i = 1; i <= numPuzzles; i++) {
       if (JSON.stringify(prevProps.puzzles[i].tileStates) !== JSON.stringify(this.props.puzzles[i].tileStates)) {
         this.props.dispatch(saveTileState(i, this.props.puzzles[i].tileStates));
+        // TODO: Check if the new condition is the winning condition
       }
     }
   }
@@ -61,20 +79,45 @@ class GamePage extends React.Component {
     dispatch(selectPuzzle(newPuzzle));
   }
 
-  handleTileChange(puzzleId, tileCoords, currentTool, event) {
+  handleBoardReset(puzzleId) {
+    this.props.dispatch(receiveTileStates(puzzleId, {}));
+  }
+
+  handleBoardDragStart(i, j, tileState, currentTool, puzzleId, event) {
     event.preventDefault();
-    const { puzzles, dispatch } = this.props;
-    if (currentTool === BLOCK && !puzzles[puzzleId].tileStates[tileCoords]) {
-      dispatch(modifyTile(puzzleId, tileCoords, BLOCK));
-    } else if (!puzzles[puzzleId].tileStates[tileCoords]) {
-      dispatch(modifyTile(puzzleId, tileCoords, HIGHLIGHT));
+
+    let tileChange;
+    if (currentTool === BLOCK && (!tileState || tileState === HIGHLIGHTED)) {
+      tileChange = BLOCK;
+    } else if (currentTool === HIGHLIGHT && (!tileState || tileState === BLOCKED)) {
+      tileChange = HIGHLIGHT;
     } else {
-      dispatch(modifyTile(puzzleId, tileCoords, EMPTY));
+      tileChange = EMPTY;
+    }
+
+    const tileCoords = `${i},${j}`;
+    this.props.dispatch(modifyTile(puzzleId, tileCoords, tileChange));
+    this.setState({
+      dragging: true,
+      dragType: tileChange
+    });
+  }
+
+  handleBoardDrag(i, j, puzzleId, event) {
+    event.preventDefault();
+    const { currentPuzzle, currentTool, puzzles } = this.props;
+    if (this.state.dragging) {
+      const tileCoords = `${i},${j}`;
+      const tileChange = this.state.dragType;
+      this.props.dispatch(modifyTile(puzzleId, tileCoords, tileChange));
     }
   }
 
-  handleBoardReset(puzzleId) {
-    this.props.dispatch(receiveTileStates(puzzleId, {}));
+  handleBoardDragEnd() {
+    this.setState({
+      dragging: false,
+      dragType: null
+    });
   }
 
   render() {
@@ -112,7 +155,8 @@ class GamePage extends React.Component {
           currentPuzzle={currentPuzzle}
           currentTool={currentTool}
           puzzles={puzzles}
-          handleTileChange={this.handleTileChange}
+          handleBoardDragStart={this.handleBoardDragStart}
+          handleBoardDrag={this.handleBoardDrag}
         />
       );
     }
